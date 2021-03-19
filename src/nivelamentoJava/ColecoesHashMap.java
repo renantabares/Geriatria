d78845c9795828a92285282a9c0253bdd0cce809
Renan <renantabares@gmail.com>
2021-03-19 15:10:29 -0300

    coleções HashMap

---
+++ src/nivelamentoJava/ColecoesHashMap.java
@@ -0,0 +1,24 @@
+package nivelamentoJava;
+
+import java.util.HashMap;
+
+public class ColecoesHashMap {
+	
+	public static void main(String[] args) {
+		
+		HashMap <String, String> chaveValor = new HashMap<String, String>();
+		chaveValor.put("chave1", "valor1");
+		chaveValor.put("chave2", "valor2");
+		chaveValor.put("chave3", "valor3");
+		
+		System.out.println(chaveValor.get("chave2"));
+		   
+		for (String key : chaveValor.keySet()) {
+
+               //Capturamos o valor a partir da chave
+               String value = chaveValor.get(key);
+               System.out.println(key + " = " + value);
+        }
+	}
+	
+}
